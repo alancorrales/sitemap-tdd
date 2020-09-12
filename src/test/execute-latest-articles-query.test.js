e50eec96ccb744934d866f7f5895122aadc9a005
Alan Corrales <alancorrales31@gmail.com>
2020-09-12 20:24:48 -0300

    Adds failure test for when there are graphQL errors

--- src/test/execute-latest-articles-query.test.js
+++ src/test/execute-latest-articles-query.test.js
@@ -1,46 +1,50 @@
-import { createTestServer } from "./test-server";
-import { executeLatestArticlesQuery } from "../execute-latest-articles-query";
-import { createTestArticlePublishedOneDayAgo } from "./data/create-test-article";
+import { createTestServer } from './test-server';
+import {
+	executeLatestArticlesQuery,
+	GraphQLError,
+} from '../execute-latest-articles-query';
+import { createTestArticlePublishedOneDayAgo } from './data/create-test-article';
 
-describe("executeLatestArticlesQuery", () => {
-  let testServer;
-  beforeAll((done) => {
-    testServer = createTestServer({ port: 4123 });
-    testServer.listen(done);
-  });
-  afterAll((done) => {
-    testServer.close(done);
-  });
-  it("executes the correct request", async () => {
-    // [...]
-  });
+describe('executeLatestArticlesQuery', () => {
+	let testServer;
+	beforeAll((done) => {
+		testServer = createTestServer({ port: 4123 });
+		testServer.listen(done);
+	});
+	afterAll((done) => {
+		testServer.close(done);
+	});
+	it('executes the correct request', async () => {
+		// [...]
+	});
 
-  it("get the latest articles from the graphQL response", async () => {
-    // arrange
-    expect.assertions(1);
-    const domain = "www.my-website.co.uk";
-    const graphQLEndpoint = "http://localhost:4123";
-    const expectedArticles = [
-      createTestArticlePublishedOneDayAgo(),
-      createTestArticlePublishedOneDayAgo(),
-      createTestArticlePublishedOneDayAgo(),
-    ];
-    testServer.setResponse({
-      status: 200,
-      body: {
-        data: {
-          latestArticles: expectedArticles,
-        },
-      },
-    });
+	it('get the latest articles from the graphQL response', async () => {
+		// [...]
+	});
 
-    // act
-    const articles = await executeLatestArticlesQuery({
-      domain,
-      graphQLEndpoint,
-    });
+	it('fails with a GraphQLError if there are graphQL errors in the response', async () => {
+		// arrange
+		expect.assertions(1);
+		const domain = 'www.my-website.co.uk';
+		const graphQLEndpoint = 'http://localhost:4123';
+		const expectedErrors = [
+			{
+				message: 'some error',
+			},
+			{
+				message: 'some other error',
+			},
+		];
+		testServer.setResponse({
+			status: 400,
+			body: {
+				errors: expectedErrors,
+			},
+		});
 
-    // assert
-    expect(articles).toEqual(expectedArticles);
-  });
+		// act & assert
+		await expect(
+			executeLatestArticlesQuery({ domain, graphQLEndpoint })
+		).rejects.toEqual(new GraphQLError(expectedErrors));
+	});
 });
