d1e19ac2219873c9fb2d58a90b91fbef87632de7
Alan Corrales <alancorrales31@gmail.com>
2020-09-13 04:35:32 -0300

    Adds nullable 'executeLatestArticlesQuery'

--- src/test/execute-latest-articles-query.test.js
+++ src/test/execute-latest-articles-query.test.js
@@ -1,50 +1,61 @@
 import { createTestServer } from './test-server';
 import {
-	executeLatestArticlesQuery,
+	createExecuteLatestArticlesQuery,
+	createFakeExecuteLatestArticlesQuery,
 	GraphQLError,
 } from '../execute-latest-articles-query';
 import { createTestArticlePublishedOneDayAgo } from './data/create-test-article';
 
 describe('executeLatestArticlesQuery', () => {
 	let testServer;
-	beforeAll((done) => {
-		testServer = createTestServer({ port: 4123 });
-		testServer.listen(done);
-	});
-	afterAll((done) => {
-		testServer.close(done);
-	});
+	beforeAll();
+	// [...]
+	afterAll();
+	// [...]
 	it('executes the correct request', async () => {
-		// [...]
+		// ...
 	});
 
 	it('get the latest articles from the graphQL response', async () => {
-		// [...]
+		// ...
 	});
 
 	it('fails with a GraphQLError if there are graphQL errors in the response', async () => {
+		// ...
+	});
+
+	it('response can be faked with predefined articles for a specific domain', async () => {
 		// arrange
-		expect.assertions(1);
-		const domain = 'www.my-website.co.uk';
-		const graphQLEndpoint = 'http://localhost:4123';
-		const expectedErrors = [
-			{
-				message: 'some error',
-			},
-			{
-				message: 'some other error',
-			},
+		expect.assertions(2);
+		const domainA = 'www.domain-a.co.uk';
+		const expectedArticlesForDomainA = [
+			createTestArticlePublishedOneDayAgo(),
+			createTestArticlePublishedOneDayAgo(),
+			createTestArticlePublishedOneDayAgo(),
 		];
-		testServer.setResponse({
-			status: 400,
-			body: {
-				errors: expectedErrors,
-			},
+		const domainB = 'www.domain-b.co.uk';
+		const expectedArticlesForDomainB = [
+			createTestArticlePublishedOneDayAgo(),
+			createTestArticlePublishedOneDayAgo(),
+			createTestArticlePublishedOneDayAgo(),
+		];
+		const executeLatestArticlesQuery = createFakeExecuteLatestArticlesQuery(
+			{
+				[domainA]: expectedArticlesForDomainA,
+				[domainB]: expectedArticlesForDomainB,
+			}
+		);
+
+		// act
+		const receivedArticlesForDomainA = await executeLatestArticlesQuery({
+			domain: domainA,
+		});
+		const receivedArticlesForDomainB = await executeLatestArticlesQuery({
+			domain: domainB,
 		});
 
-		// act & assert
-		await expect(
-			executeLatestArticlesQuery({ domain, graphQLEndpoint })
-		).rejects.toEqual(new GraphQLError(expectedErrors));
+		// assert
+		expect(receivedArticlesForDomainA).toEqual(expectedArticlesForDomainA);
+		expect(receivedArticlesForDomainB).toEqual(expectedArticlesForDomainB);
 	});
 });
